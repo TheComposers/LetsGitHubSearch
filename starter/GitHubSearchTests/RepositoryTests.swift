06ef5d036bebb6c36ba08a13c63039bcdb0aeb85
Daeyoung Kim <daykim22@gmail.com>
2022-11-28 15:18:00 +0900

    Rename test
    Signed-off-by: Daeyoung Kim <daykim22@gmail.com>

--- starter/GitHubSearchTests/RepositoryTests.swift
+++ starter/GitHubSearchTests/RepositoryTests.swift
@@ -6,12 +6,12 @@ import ComposableArchitecture
 
 @MainActor
 final class RepositoryTests: XCTestCase {
-  func test_searchResults_whenSearch() async {
-    // # Arrange
+  func test_user_get_repoSearchResults_when_search() async {
+    // Arrange
     // TestStore를 생성합니다.
 
-    // # Act & Assert
-    // 1. Swift 키워드가 입력됐을 때, store의 `keyword` state가 "Swift"인지 테스트합니다.
+    // Act & Assert
+    // 1. "Swift"를 입력했을 때, state.keyword가 "Swift"인지 테스트합니다.
     // 2. 검색을 했을 때, 예상하는 검색 결과가 나오는지를 테스트합니다.
   }
 }
