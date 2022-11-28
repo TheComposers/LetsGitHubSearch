d4f36e53320ec5dfb7f329258846e34647dec101
Daeyoung Kim <daykim22@gmail.com>
2022-11-28 21:00:49 +0900

    Add test case
    Signed-off-by: Daeyoung Kim <daykim22@gmail.com>

--- final/GitHubSearchTests/RepositoryTests.swift
+++ final/GitHubSearchTests/RepositoryTests.swift
@@ -34,4 +34,29 @@ final class RepositoryTests: XCTestCase {
       ]
     }
   }
+
+  func test_requestCount_when_keywordClearedWithinDebounceTime() async {
+    let store = TestStore(
+      initialState: RepoSearch.State(),
+      reducer: RepoSearch()
+    )
+
+    store.dependencies.repoSearchClient.search = { _ in .mock }
+
+    let clock = TestClock()
+    store.dependencies.continuousClock = clock
+
+    await store.send(.keywordChanged("Swift")) {
+      $0.keyword = "Swift"
+    }
+
+    await clock.advance(by: .seconds(0.3))
+
+    await store.send(.keywordChanged("")) {
+      $0.keyword = ""
+      $0.requestCount = 0
+      $0.isLoading = false
+      $0.searchResults = []
+    }
+  }
 }
