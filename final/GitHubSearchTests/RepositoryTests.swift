ffd77d60a8589383d34c54f74310c98d976323e9
Daeyoung Kim <daykim22@gmail.com>
2022-11-28 16:28:01 +0900

    Add test
    Signed-off-by: Daeyoung Kim <daykim22@gmail.com>

--- final/GitHubSearchTests/RepositoryTests.swift
+++ final/GitHubSearchTests/RepositoryTests.swift
@@ -7,11 +7,22 @@ import ComposableArchitecture
 @MainActor
 final class RepositoryTests: XCTestCase {
   func test_user_get_repoSearchResults_when_search() async {
-    // Arrange
-    // TestStore를 생성합니다.
+    let store = TestStore(
+      initialState: RepoSearch.State(),
+      reducer: RepoSearch()
+    )
 
-    // Act & Assert
-    // 1. "Swift"를 입력했을 때, state.keyword가 "Swift"인지 테스트합니다.
-    // 2. 검색을 했을 때, 예상하는 검색 결과가 나오는지를 테스트합니다.
+    await store.send(.keywordChanged("Swift")) {
+      $0.keyword = "Swift"
+    }
+
+    await store.send(.search) {
+      $0.searchResults = [
+        "Swift",
+        "SwiftyJSON",
+        "SwiftGuide",
+        "SwiftterSwift",
+      ]
+    }
   }
 }
